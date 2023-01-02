f0b90079b05f871ee9e87efd4f574c016511bde8
Macanijus <tmacan975@gmail.com>
2023-01-02 17:05:26 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const jsonData = require("./pokedex.json");
+const cors = require("cors");
+
+const PORT = process.env.PORT || 9000;
+
+app.use(express.json());
+
+app.get("/pokemon", (req, res) => {
+  res.send(jsonData);
+});
+
+app.get("/pokemon/:id", (req, res) => {
+  res.send(jsonData[req.params.id - 1]);
+});
+
+app.get("/pokemon/:id/:info", (req, res) => {
+  res.send(jsonData[req.params.id - 1][req.params.info]);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on http://localhost:${PORT}`);
+});
